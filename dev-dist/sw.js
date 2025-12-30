bbd91cbb3d7b059805f36c8e6b6df624c62b7a05
alimardonc <ortiqboyevalimardon@gmail.com>
2025-12-30 21:01:30 +0500

    >

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0sn0f355t7k"
+    "revision": "0.8jqgsd5g73g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
